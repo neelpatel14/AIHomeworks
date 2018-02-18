b70215fc6ef9ceb25115f4d564b1f06f2656a139
Anirudh Ryali <aryali97@gmail.com>
2018-02-18 02:21:41 -0500

    Added tree file (nothing in there)

---
+++ HW2/tree.py
@@ -0,0 +1,10 @@
+import random
+import Queue
+from copy import deepcopy
+
+
+def construct_tree(string):
+    
+
+if __name__ == "__main__":
+    logic_string = raw_input("Enter a well-formed statement")
