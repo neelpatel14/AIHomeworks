55381a1db754f43ed6a9730cb94438aa4c7158c6
Anirudh Ryali <aryali97@gmail.com>
2018-04-05 02:56:49 -0400

    Added simple json setup

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -1,4 +1,6 @@
 import random
+import json
+import os.path
 
 TEAM_NAME = "ani and friends"
 MEMBERS = ["np2ch","ar9fh","aml5ha"] #Include a list of your members’ UVA IDs
@@ -13,24 +15,31 @@ state = {
 	"last-opponent-play": 0.71,
 	"last-outcome": -10,
 }
+info_filename = 'info.json'
 
 # This will load whatever dictionary you last saved, stub included to drive example,
 #   isn't actual function
 def load_info():
-	return {}
+    info = {}
+    if os.path.isfile(info_filename):
+        with open(info_filename, 'r') as info_file:
+            info = json.load(info_file)
+    return info
 
 # This will save (and overwrite) whatever dictionary you last saved
 # Stub included to drive example, isn't actual function
 def save_info(info):
-	new_info  = load_info() # load the previous data
-
+    new_info = load_info()
+    new_info.update(info)
+    with open(info_filename, 'w') as info_file:
+        json.dump(new_info, info_file)
+"""
 	if state["game"] == "chicken":
 		#add new chicken info to old data
 
 	if state["game"] =="connect_more":
 		#add new connect_more data to old data
-
-
+"""
 
 # returns a random move (for sake of example)
 def get_chicken_move(state):
@@ -59,4 +68,8 @@ def get_move(state):
 	if state["game"] == "connect_more":
 		return get_connect_move(state)
 
-print (get_move(state))
+#print (get_move(state))
+
+if __name__ == '__main__':
+    print(load_info())
+    save_info({'a': 2})
